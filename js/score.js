1ae5335f5bef2f2af09db7de581e6606cb2fd2e2
PotatoDaMan <105178860+PotatoDaMan@users.noreply.github.com>
2024-03-24 11:13:35 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-53.5*Math.pow(rank-1, 0.45) + 300) *
+    let score = (-53.5*Math.pow(rank-1, 0.4) + 300) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
